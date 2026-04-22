33c12ab1b9a390be8929f460e9034f336e0d6b87
br788 <gaelbrian2021@gmail.com>
2026-04-22 23:07:11 +0200

    Initial project agribridge

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class DashboardController extends Controller
+{
+    //
+    public function index()
+    {
+        $user = auth()->user();
+        $role = $user->role;
+
+        return view("dashboard.{$role}"); // afficher la vue correspondante 
+    }
+}
